oprypin/rebrickable-refined

commit 8171018239a5ece3b193941aed0ff35fc734e3b8

Add ability to sort alternate builds

src/sort-utils.ts
@@ -1,0 +1,36 @@
+// Copyright (C) 2026 Oleh Prypin
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+function naturalSortKey(value: string | null | undefined): Array<string | number> {
+    if (!value) {
+        return [];
+    }
+    const valueArr: Array<string | number> = value.split(/((?:0|[1-9][0-9]*)(?:\.[0-9]+)?)/);
+    for (let i = 1; i < valueArr.length; i += 2) {
+        valueArr[i] = +valueArr[i];
+    }
+    return valueArr;
+}
+
+function sortBy<T>(arr: Array<T>, key: (arg: T) => any): Array<T> {
+    return arr.sort((a, b) => cmpRespectingArrays(key(a), key(b)));
+}
+
+// Returns -1, 0 or 1 based on the comparison of the two values.
+// In the case of arrays, an item-by-item lexicographic comparison is implemented.
+function cmpRespectingArrays(a, b): -1 | 0 | 1 {
+    if (Array.isArray(a) && Array.isArray(b)) {
+        for (let i = 0; i < Math.min(a.length, b.length); ++i) {
+            const result = cmpRespectingArrays(a[i], b[i]);
+            if (result !== 0) {
+                return result;
+            }
+        }
+        return a.length > b.length ? 1 : -1;
+    } else if (a === b) {
+        return 0;
+    } else {
+        return a > b ? 1 : -1;
+    }
+}

src/common.ts
@@ -7,7 +7,13 @@ function createElement<K extends keyof HTMLElementTagNameMap>(
 ): HTMLElementTagNameMap[K] {
     const el = document.createElement(tagName);
     for (const key in options) {
-        el[key] = options[key]!;
+        if (key === 'dataset') {
+            for (const [k, v] of Object.entries(options[key] as Record<string, string>)) {
+                el.dataset[k] = v;
+            }
+        } else {
+            el[key] = options[key]!;
+        }
     }
     if (children) {
         el.append(...children);

src/popup.html
@@ -178,6 +178,7 @@
 
     <h3>Set / MOC details</h3>
     <label><input type="checkbox" id="redesign-set-and-moc-tiles"> Redesign set and MOC tiles: show likes, release dates, part counts</label>
+    <label><input type="checkbox" id="moc-sort-options"> Allow sorting MOCs in the Alt Builds tab</label>
     <label><input type="checkbox" id="detailed-moc-sidebar"> Show more details on the sidebar of a MOC</label>
     <label><input type="checkbox" id="quick-download-csv-link"> Add a quick download link for the CSV parts list in the sidebar of a MOC</label>
 

src/set-lists.ts
@@ -1,7 +1,11 @@
 // Copyright (C) 2026 Oleh Prypin
 
+function getAuthorName(setEl: Element): string | null {
+    return setEl.querySelector<HTMLAnchorElement>('a[href^="/mocs/"]')?.href.match(/\/mocs\/MOC-[0-9]+\/([^/]+)/)?.[1] ?? null;
+}
+
 function populateSetDetails(root: HTMLDivElement) {
-    const details = root.querySelector('.set-tn-details>*');
+    const details = root.querySelector<HTMLElement>('.set-tn-details>*');
     if (details == null || details.classList.contains('rbrefined-populated') || details.closest('.set-tn-small')) {
         return;
     }
@@ -19,11 +23,11 @@ function populateSetDetails(root: HTMLDivElement) {
     }
     if (authorEl == null) {
         // Populate author in case it's missing, also adjust vertical spacing for build search page.
-        const match = details.querySelector<HTMLAnchorElement>('a[href^="/mocs/"]')?.href.match(/\/mocs\/MOC-[0-9]+\/([^/]+)/);
+        const authorName = getAuthorName(details);
         const authorEl = createElement('div', {className: 'clearfix trunc mb-6 size-12 rbrefined-set-author'}, ['\u00a0']);
-        if (match != null && !document.location.pathname.startsWith(`/users/${match[1]}/`)) {
-            const authorLink = createElement('a', {className: 'js-hover-card', href: `/users/${match[1]}/mocs/`}, [decodeURIComponent(match[1])]);
-            authorLink.dataset['hover'] = `/users/${match[1]}/card/`;
+        if (authorName != null && !document.location.pathname.startsWith(`/users/${authorName}/`)) {
+            const authorLink = createElement('a', {className: 'js-hover-card', href: `/users/${authorName}/mocs/`}, [decodeURIComponent(authorName)]);
+            authorLink.dataset['hover'] = `/users/${authorName}/card/`;
             authorEl.replaceChildren('By ', authorLink);
             result.push(authorEl);
         }
@@ -149,6 +153,88 @@ function populateSetDetails(root: HTMLDivElement) {
     details.replaceChildren(...result);
     details.classList.add('rbrefined-populated');
 }
+
+when('moc-sort-options', (activated) => {
+    for (const container of document.querySelectorAll<HTMLElement>('#tab_alt_builds')) {
+        observeChanges(container, () => {
+            if (!container.querySelector('.set-tn')) {
+                return false;
+            }
+            const sortOptionElements: Array<HTMLLIElement> = [];
+            for (const [sortByName, sortByKey, sortDefault] of [
+                ['Likes', 'likes', 'D'],
+                ['Year', 'added', 'A'],
+                ['Num Parts', 'num_parts', 'D'],
+                ['Author', 'author', 'A'],
+            ]) {
+                const li = createElement('li', {dataset: {sortByKey, sortDefault}}, [ // 'sort1': 'color_name', 'sort2': 'part_name', 'sort_by': '0',
+                    createElement('a', {}, [
+                        createElement('i', {className: 'fa fa-fw'}),
+                        ' ', sortByName,
+                    ]),
+                ]);
+                sortOptionElements.push(li);
+            }
+
+            const sortSelector = createElement('div', {className: 'btn-group', title: 'Sort MOCs'}, [
+                createElement('button', {type: 'button', className: 'btn btn-default btn-sm dropdown-toggle', dataset: {'toggle': 'dropdown'}}, [
+                    createElement('span'),
+                    ' ', createElement('span', {className: 'caret'}),
+                ]),
+                createElement('ul', {className: 'dropdown-menu', role: 'menu'}, sortOptionElements),
+            ]);
+            container.prepend(sortSelector);
+
+            nestedEventListener(sortSelector, 'li', 'click', (e, target) => {
+                const {sortByKey, sortDefault} = target.dataset;
+                const thisI = target.querySelector('i')!;
+                const isDescending = !(
+                    sortDefault === 'D'
+                        ? thisI.classList.contains('fa-sort-amount-desc')
+                        : !thisI.classList.contains('fa-sort-amount-asc')
+                );
+                for (const otherI of sortSelector.querySelectorAll('i')) {
+                    otherI.classList.remove('fa-sort-amount-desc');
+                    otherI.classList.remove('fa-sort-amount-asc');
+                }
+                thisI.classList.add(isDescending ? 'fa-sort-amount-desc' : 'fa-sort-amount-asc');
+                sortSelector.querySelector('span')!.replaceChildren(...target.querySelector('a')!.cloneNode(true).childNodes);
+
+                for (const heading of container.querySelectorAll<HTMLDivElement>('div.heading-title')) {
+                    heading.style.marginTop = '20px';
+                    heading.style.marginBottom = '20px';
+
+                    const elementsToSort: Array<Element> = [];
+                    let el: Element | null = heading;
+                    while ((el = el.nextElementSibling) && el.querySelector('div.heading-title') == null) {
+                        if (el.querySelector('div.js-sort-data')) {
+                            elementsToSort.push(el);
+                        }
+                    }
+                    if (elementsToSort.length <= 1) {
+                        continue;
+                    }
+                    sortBy(elementsToSort, (e) => {
+                        const data = e.querySelector<HTMLDivElement>('div.js-sort-data')!.dataset;
+                        return [
+                            sortByKey === 'author' ? getAuthorName(e)?.toLocaleLowerCase() : naturalSortKey(data[sortByKey!]),
+                            naturalSortKey(data['added']),
+                        ];
+                    });
+                    if (isDescending) {
+                        elementsToSort.reverse();
+                    }
+                    elementsToSort[0].before(...elementsToSort);
+                }
+            });
+
+            sortOptionElements[0].click();
+
+            activated();
+            return true;
+        });
+    }
+});
 
 when('redesign-set-and-moc-tiles', (activated) => {
     for (const container of document.querySelectorAll<HTMLElement>('#set_list_sets, #tab_sets, #tab_alt_builds, #filtered_results, #designer_mocs, #related_mocs, #build_results')) {

src/settings.ts
@@ -18,6 +18,7 @@ const settings = {
     'part-dialog-always-sort-colors': true,
     'part-dialog-improved-keyboard-input': true,
     'redesign-set-and-moc-tiles': true,
+    'moc-sort-options': true,
     'detailed-moc-sidebar': true,
     'quick-download-csv-link': true,
     'increase-image-resolution': false,

src/sort-order.ts
@@ -16,24 +16,6 @@ function getWindow() {
 
 /* eslint-disable camelcase */
 
-// Returns -1, 0 or 1 based on the comparison of the two values.
-// In the case of arrays, an item-by-item lexicographic comparison is implemented.
-function cmpRespectingArrays(a, b) {
-    if (Array.isArray(a) && Array.isArray(b)) {
-        for (let i = 0; i < Math.min(a.length, b.length); ++i) {
-            const result = cmpRespectingArrays(a[i], b[i]);
-            if (result !== 0) {
-                return result;
-            }
-        }
-        return a.length - b.length;
-    } else if (a === b) {
-        return 0;
-    } else {
-        return a > b ? 1 : -1;
-    }
-}
-
 function sortItems(containerJ, itemsJ, dataSelector: string, sort1: string, sort2: string, sortDir: 'A' | 'D') {
     const [container]: [HTMLElement] = $(containerJ);
     if (container == null) {
@@ -63,21 +45,16 @@ function sortItems(containerJ, itemsJ, dataSelector: string, sort1: string, sort
         if (sortType === 'color_hsv') {
             return [value, data('color_name').includes('Trans') ? 0 : 1];
         }
-        // Implement natural sort order.
-        const valueArr: Array<string | number> = value.split(/(0|[1-9][0-9]*)/);
-        for (let i = 1; i < valueArr.length; i += 2) {
-            valueArr[i] = +valueArr[i];
-        }
-        return valueArr;
+        return naturalSortKey(value);
     }
 
     items.sort((a: HTMLElement, b: HTMLElement) => {
         for (const [i, so] of sortOrder.entries()) {
-            let cmpResult = cmpRespectingArrays(getSortKey(a, so), getSortKey(b, so));
-            if (sortDir === 'D' && i === 0) {
-                cmpResult = -cmpResult;
-            }
+            const cmpResult = cmpRespectingArrays(getSortKey(a, so), getSortKey(b, so));
             if (cmpResult) {
+                if (sortDir === 'D' && i === 0) {
+                    return -cmpResult;
+                }
                 return cmpResult;
             }
         }

commit f17a3f74f2e27b66ac77549658f352ac1ac2d8b9

Fix a bug with sorting, where equal items would show up as unequal
Bug introduced in commit 8171018239a5ece3b193941aed0ff35fc734e3b8

src/sort-utils.ts
@@ -19,7 +19,7 @@ function sortBy<T>(arr: Array<T>, key: (arg: T) => any): Array<T> {
 
 // Returns -1, 0 or 1 based on the comparison of the two values.
 // In the case of arrays, an item-by-item lexicographic comparison is implemented.
-function cmpRespectingArrays(a, b): -1 | 0 | 1 {
+function cmpRespectingArrays(a, b): number {
     if (Array.isArray(a) && Array.isArray(b)) {
         for (let i = 0; i < Math.min(a.length, b.length); ++i) {
             const result = cmpRespectingArrays(a[i], b[i]);
@@ -27,7 +27,7 @@ function cmpRespectingArrays(a, b): -1 | 0 | 1 {
                 return result;
             }
         }
-        return a.length > b.length ? 1 : -1;
+        return a.length - b.length;
     } else if (a === b) {
         return 0;
     } else {