64994bf1e73402cc8ca5f5e849e1d985770be959
Oleh Prypin <oleh@pryp.in>
2026-09-17 16:40:11 +0200

    Drop redundant style

--- src/part-lists.ts
+++ src/part-lists.ts
@@ -145,8 +145,6 @@ body.text-high-contrast .rbrefined-part-list .js-part .js-part-price {
 }
 `.replace(/^\.\b/gm, 'body .rbrefined-part-list .') + /* css */ `
 .js-part .part-text__qty {
-    font-size: 115%;
-    font-weight: 600;
     margin-left: 0.2ex;
 }
 .js-part .part-text__qty::after {
